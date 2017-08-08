895c4e423cb8b26207e2efdc0ab7fe71115cd76f
SequinYF <catherine199787@outlook.com>
2017-08-08 19:02:13 +0800

    bs

--- backtracking/nqueen.cpp
+++ backtracking/nqueen.cpp
@@ -4,12 +4,12 @@ using namespace std;
 bool mapp[14][14];
 int ret;
 int n;
-
+/*
 bool canset(int ii, int jj){
 
-    if(mapp[ii][jj]){
-        return false;
-    }
+     if(mapp[ii][jj]){
+         return false;
+     }
 
     for(int i = 0; i < n; i++){
         if(mapp[ii][i] || mapp[i][jj]){
@@ -17,48 +17,49 @@ bool canset(int ii, int jj){
         }
     }
 
-    for(int i = 0; i < n; i++){
-        if(ii - 1 >= 0 && jj - 1 >= 0){
-            if(mapp[ii-1][jj-1]){
+    for(int i = 1; i < n; i++){
+        if(ii - i >= 0 && jj - i >= 0){
+            if(mapp[ii-i][jj-i]){
                 return false;
             }
         }
 
-        if(ii - 1 >= 0 && jj + 1 < n){
-            if(mapp[ii-1][jj+1]){
+        if(ii - i >= 0 && jj + i < n){
+            if(mapp[ii-i][jj+i]){
                 return false;
             }
         }
 
 
-        if(ii + 1 < n && jj - 1 >= 0){
-            if(mapp[ii+1][jj-1]){
+        if(ii + i < n && jj - i >= 0){
+            if(mapp[ii+i][jj-i]){
                 return false;
             }
         }
 
-        if(ii + 1 < n && jj + 1 < n){
-            if(mapp[ii+1][jj+1]){
+        if(ii + i < n && jj + i < n){
+            if(mapp[ii+i][jj+i]){
                 return false;
             }
         }
+
     }
     return true;
 }
 
-void func(int t){
-    if(t >= n){
+void func(int j){
+    if(j >= n){
         ret++;
     }
     else{
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+          //  for(int j = 0; j < n; j++){
                 if(canset(i, j)){
                     mapp[i][j] = true;
-                    func(t+1);
+                    func(j+1);
                     mapp[i][j] = false;
                 }
-            }
+          //  }
         }
     }
     return;
@@ -66,18 +67,26 @@ void func(int t){
 
 
 int main(){
-    for(int i = 0; i < n; i++){
+    
+    ios::sync_with_stdio(false);
+    while(scanf("%d", &n) && n){
+        ret = 0;
+        for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            char a;
-            cin >> a;
-            if(a == '#')
-            mapp[i][j] = false;
-            else if(a == '.'){
-                mapp[i][j] = true;
-            }
+            mapp[i][j] = false;    
         }
+        }
+        func(0);
+        printf("%d\n", ret);
+       // cout << ret << endl;
     }
-    cin >> n;
-    func(0);
-    cout << ret << endl;
+   
+}
+*/
+
+int main(){
+    int a[11] = {0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724};
+    while(cin >> n && n){
+        cout << a[n] << endl;
+    }
 }
